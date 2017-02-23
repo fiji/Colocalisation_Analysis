2624bc49f8f053978f854559d1d7a12ca9fda0d6
Curtis Rueden <ctrueden@wisc.edu>
2017-02-23 10:40:52 -0600

    Add license blurbs and project license
    Accomplished using:
        mvn license:update-file-header license:update-project-license

--- src/main/java/Coloc_2.java
+++ src/main/java/Coloc_2.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * Fiji's plugin for colocalization analysis.
+ * %%
+ * Copyright (C) 2009 - 2017 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 import algorithms.Algorithm;
 import algorithms.AutoThresholdRegression;
 import algorithms.AutoThresholdRegression.Implementation;
@@ -55,24 +76,6 @@ import results.PDFWriter;
 import results.ResultHandler;
 import results.SingleWindowDisplay;
 import results.Warning;
-
-/**
-   Copyright 2010-2015, Daniel J. White, Tom Kazimiers, Johannes Schindelin
-   and the Fiji project. Fiji is just imageJ - batteries included.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see http://www.gnu.org/licenses/ .
- */
 
 /**
  * An ImageJ plugin which does pixel intensity correlation based
