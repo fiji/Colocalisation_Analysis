4664f9595314e585ae62d8598bd90a6ae10f7a8e
Curtis Rueden <ctrueden@wisc.edu>
2017-05-04 12:32:59 -0500

    Main: avoid 'Unrecognized command' error
    The recent changes to the codebase somehow broke the main method.
    My guess is that the switch to sc.fiji.coloc package prefix is
    somehow making ImageJ1 unhappy, but we do set the plugins.dir
    appropriately. The plugins.config is there, and correct. So I do
    not know why it cannot find the command. To work around the
    issue, we use IJ.runPlugIn(className, arg) instead, which works
    fine. And then we no longer need to override the plugins.dir.

--- src/test/java/sc/fiji/coloc/Main.java
+++ src/test/java/sc/fiji/coloc/Main.java
@@ -43,12 +43,6 @@ public class Main {
 	 * @param args unused
 	 */
 	public static void main(String[] args) {
-		// set the plugins.dir property to make the plugin appear in the Plugins menu
-		Class<?> clazz = Coloc_2.class;
-		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
-		String pluginsDir = url.substring(5, url.length() - clazz.getName().length() - 6);
-		System.setProperty("plugins.dir", pluginsDir);
-
 		// start ImageJ
 		new ImageJ();
 
@@ -58,7 +52,7 @@ public class Main {
 		fluorCellchannels[0].show();
 		fluorCellchannels[1].show();
 		// run the plugin, Coloc 2
-		IJ.run("Coloc 2","channel_1=C1-FluorescentCells.tif channel_2=C2-FluorescentCells.tif roi_or_mask=<None> threshold_regression=Costes display_images_in_result li_histogram_channel_1 li_histogram_channel_2 li_icq spearman's_rank_correlation manders'_correlation kendall's_tau_rank_correlation 2d_instensity_histogram costes'_significance_test psf=3 costes_randomisations=10");
+		IJ.runPlugIn(Coloc_2.class.getName(),"channel_1=C1-FluorescentCells.tif channel_2=C2-FluorescentCells.tif roi_or_mask=<None> threshold_regression=Costes display_images_in_result li_histogram_channel_1 li_histogram_channel_2 li_icq spearman's_rank_correlation manders'_correlation kendall's_tau_rank_correlation 2d_instensity_histogram costes'_significance_test psf=3 costes_randomisations=10");
 
 //		// open the Confocal Series sample (to test z-stacks)
 //		ImagePlus confocalImage = IJ.openImage("http://imagej.net/images/confocal-series.zip");
@@ -66,6 +60,6 @@ public class Main {
 //		confocalchannels[0].show();
 //		confocalchannels[1].show();
 //		// run the plugin, Coloc 2
-//		IJ.run("Coloc 2", "channel_1=C1-confocal-series.tif channel_2=C2-confocal-series.tif roi_or_mask=<None> threshold_regression=Costes display_images_in_result li_histogram_channel_1 li_histogram_channel_2 li_icq spearman's_rank_correlation manders'_correlation kendall's_tau_rank_correlation 2d_instensity_histogram costes'_significance_test psf=3 costes_randomisations=10");
+//		IJ.runPlugIn(Coloc_2.class.getName(), "channel_1=C1-confocal-series.tif channel_2=C2-confocal-series.tif roi_or_mask=<None> threshold_regression=Costes display_images_in_result li_histogram_channel_1 li_histogram_channel_2 li_icq spearman's_rank_correlation manders'_correlation kendall's_tau_rank_correlation 2d_instensity_histogram costes'_significance_test psf=3 costes_randomisations=10");
 	}
 }
